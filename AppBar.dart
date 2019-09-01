1c3c52c0d23281e6f9117fc24bac78772058f0b0
JEN-YC <gn9635201@gmail.com>
2019-09-01 12:55:03 +0800

    Create AppBar.dart

---
+++ AppBar.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hello Flutter',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  TabController _tabController;
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: tabList.length,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("AppBar Title"),
+          leading: Icon(FontAwesomeIcons.dragon),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(FontAwesomeIcons.search),
+              onPressed: null,
+            )
+          ],
+          bottom: TabBar(
+            tabs: tabList.map((choice) {
+              return Tab(
+                text: choice.title,
+                icon: Icon(choice.icon),
+              );
+            }).toList(),
+            controller: _tabController,
+          ),
+        ),
+        body: TabBarView(
+            children: tabList.map((choice) {
+          return Center(
+            child: Icon(
+              choice.icon,
+              size: 100.0,
+            ),
+          );
+        }).toList()),
+      ),
+    );
+  }
+}
+
+class TabChoice {
+  final String title;
+  final IconData icon;
+  const TabChoice(this.title, this.icon);
+}
+
+const List<TabChoice> tabList = const <TabChoice>[
+  TabChoice('Happy', Icons.mood),
+  TabChoice('Sad', Icons.mood_bad),
+];
